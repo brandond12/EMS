2af81c0f6fc78949f798ec2f93135fd39713e321
shikodingo <shikodingo@gmail.com>
2015-11-18 14:20:50 -0500

    Added in method headers to all methods in the UIMenu.cs file.

--- EMS/EMS/UIMenu.cs
+++ EMS/EMS/UIMenu.cs
@@ -9,38 +9,126 @@ namespace EMS
     static class UIMenu
     {
         TheCompany company = new TheCompany();
+
+        /**
+        * \brief The ShowMainMenu method will display to the user, a UI menu.This menu 
+        * allows the user to choose either to manage EMS database files, manage employees,
+        * or to quit the program.  
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - n/a
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        * 
+        * \return n/a
+        */
         public static void ShowMainMenu()
         {
 
         }
 
-        public static void ShowFIleManagmentMenu()
+        /**
+        * \brief The ShowFileManagementMenu method will display to the user, a UI menu.This 
+        * menu allows the user to choose either to load the EMS database from the file, save 
+        * an employee set to the EMS database, or return to the main menu.
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - n/a
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        * 
+        * \return n/a
+        */
+        public static void ShowFileManagementMenu()
         {
 
         }
 
-        public static void ShowEmployeeManagmentMenu()
+        /**
+        * \brief The ShowEmployeeManagementMenu method will display to the user, a UI menu.This 
+        * menu allows the user to choose either to display an employee set, create a new employee,
+        * modify and exisiting employee, remove an existing employee, or return to the main menu.
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - n/a
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        * 
+        * \return n/a
+        */
+        public static void ShowEmployeeManagementMenu()
         {
 
         }
 
+        /**
+        * \brief The ShowEmployeeDetailsMenu method will display to the user, a UI menu.This 
+        * menu allows the user to choose either to specify base employee details, to specify
+        * values for any employee type, or return to the employee management menu.
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - n/a
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        * 
+        * \return n/a
+        */
         public static void ShowEmployeeDetailsMenu()
         {
 
         }
 
+        /**
+        * \brief The ShowFindEmployeeMenu method will display to the user, a UI menu.This 
+        * menu allows the user to search for a specific employee in the database. 
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - n/a
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        * 
+        * \return - AllEmployees.Employee - returns an employee object 
+        */
         public static AllEmployees.Employee ShowFindEmployeeMenu()
         {
             //select a method of finding an employee
             return new AllEmployees.Employee();
         }
 
+        /**
+        * \brief The GetInfoFromUser method will get the users input and return it 
+        * to further process it. 
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - string UserPrompt - contains the users input
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        * 
+        * \return - string UserPrompt - returns the users information
+        */
         public static String GetInfoFromUser(String UserPrompt)
         {
 
             return " "; //temp to remove errors
         }
 
+        /**
+        * \brief The ShowError method displays an appropriate error message to the user. 
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - string errorMessage - contains the message
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        * 
+        * \return - n/a
+        */
         public static void ShowError(String errorMessage)
         {
 
