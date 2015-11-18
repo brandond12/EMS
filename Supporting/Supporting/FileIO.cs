517cb11ebd81bfbb2c756596d3a0a9e544b9e8a7
Nathan-Nickel <nthnnckl@yahoo.ca>
2015-11-18 15:11:19 -0500

    Added comments to all methods in FileIO.cs all method header commenting in the FileIO.cs should be complete no coding has been done yet

--- Supporting/Supporting/FileIO.cs
+++ Supporting/Supporting/FileIO.cs
@@ -9,7 +9,15 @@ namespace Supporting
 {
     class FileIO
     {
-        //Give file name, return list of all valid records
+        /**
+        * \brief Give file name, return list of all valid records
+        *
+        * \details <b>Details</b>
+        *
+        * \param fileName - <b>string</b> - The file path and name of file storing the records
+        *
+        * \return  employeeRec - <b>List<AllEmployees.Employee></b> - The list of all the employee records in the file
+        */
         public List<AllEmployees.Employee> ReadAllRecords(String fileName)
         {
             //open file
@@ -17,10 +25,20 @@ namespace Supporting
             //pars data
             //validate data
             //return list of valid employees
-            return new List<AllEmployees.Employee>();//temp for removing errors
+            List<AllEmployees.Employee> employeeRec = new List<AllEmployees.Employee>();
+            return employeeRec;
         }
 
-        //Give employee write and file to write to
+        /**
+        * \brief Give employee write and file to write to
+        *
+        * \details <b>Details</b>
+        *
+        * \param Employee - <b>AllEmployees.Employee</b> - The employees records
+        * \param fileName - <b>String</b> - The file path and name of file storing the records
+        *
+        * \return  n/a
+        */
         public void WriteRecord(AllEmployees.Employee Employee, String fileName)
         {
             //open file 
@@ -28,30 +46,66 @@ namespace Supporting
             //close file
         }
 
+        /**
+        * \brief Give file name, return list of all valid records
+        *
+        * \details <b>Details</b>
+        *
+        * \param fileName - <b>string</b> - The file path and name of file storing the records
+        *
+        * \return readFile - <b>FileStream</b> - The opened file to be read
+        */
         private FileStream OpenFileForRead(String fileName)
         {
-            IntPtr temp = new IntPtr(0);// temp to remove errors
-            return new FileStream(temp, FileAccess.Read); // temp to remove errors
+            FileStream readFile = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            return readFile;
         }
 
+        /**
+        * \brief Give file name, return list of all valid records
+        *
+        * \details <b>Details</b>
+        *
+        * \param fileName - <b>string</b> - The file path and name of file storing the records
+        *
+        * \return writeFile - <b>FileStream</b> - The opened file to be written to
+        */
         private FileStream OpenFileForWrite(String fileName)
         {
-            IntPtr temp = new IntPtr(0);// temp to remove errors
-            return new FileStream(temp, FileAccess.Write); // temp to remove errors
+            FileStream writeFile = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            return writeFile;
         }
 
+        /**
+        * \brief Closes the file
+        *
+        * \details <b>Details</b>
+        *
+        * \param file - <b>FileStream</b> - The file to be closed
+        *
+        * \return  n/a
+        */
         private void CloseFile(FileStream file)
         {
 
         }
 
-        //given string from file, pars all data into list, return list valid employees
+        /**
+        * \brief given string from file, pars all data into list, return list valid employees
+        *
+        * \details <b>Details</b>
+        *
+        * \param fileText - <b>string</b> - The string of data containing an employees records
+        *
+        * \return  employeeRec - <b>List<AllEmployees.Employee></b> - The list of all the employee records in the strinng of data
+        */
         private List<AllEmployees.Employee> ParsRecord(String fileText)
         {
             //loop through string of data
             //parsing data
             //save all records to list
-            return new List<AllEmployees.Employee>();//temp for removing errors
+            List<AllEmployees.Employee> employeeRec = new List<AllEmployees.Employee>();
+            return employeeRec;
         }
     }
 }
