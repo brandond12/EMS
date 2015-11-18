0c5e0083049f7cb9872489b6aab71c26b5e891aa
shikodingo <shikodingo@gmail.com>
2015-11-18 14:35:25 -0500

    Added method header comments to the Logging.cs file.

--- Supporting/Supporting/Logging.cs
+++ Supporting/Supporting/Logging.cs
@@ -10,17 +10,53 @@ namespace Supporting
 {
     public static class Logging
     {
+
+        /**
+        * \brief The LogEvent method will log each step the user takes. 
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - string className - contains the class name 
+        * \param args - string methodName - contains the method name being used
+        * \param args - string eventDetails - contains the events details
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        * 
+        * \return - n/a 
+        */
         public static void LogEvent(String className, String methodName, String eventDetails)
         {
             //example in winProg-M08 slides
             //I am a little shy onthe details on how the event logger class works. The arguments may have to be tweaked
         }
 
+        /**
+        * \brief The OpenLog method will open each log. 
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - n/a
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        *
+        * \return - EventLog - returns the log object 
+        */
         private static EventLog OpenLog()
         {
             return new EventLog();//temp to remove erros
         }
 
+        /**
+        * \brief The CloseLog method will close each log. 
+        * 
+        * \details <b>Details</b>
+        * 
+        * \param args - EventLog Log - contains the log file.
+        * 
+        * \throw <EndOfProgramException> - If the user wants the program to end
+        *
+        * \return - n/a
+        */
         private static void CloseLog(EventLog Log)
         {
 
